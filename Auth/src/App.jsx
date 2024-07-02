4c4e7e4b77b3a56999379aebdfed63ccee1670b3
ChethanJS07 <jschethan75@gmail.com>
2024-07-02 19:38:58 +0530

    Added the route to the error page

--- Auth/src/App.jsx
+++ Auth/src/App.jsx
@@ -1,21 +1,21 @@
-import { useState } from 'react'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
-import Login from './login'
-import Register from './register'
-import Home from './homepage'
-
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Login from './login';
+import Register from './register';
+import Home from './homepage';
+import Error from './errorpage';
 function App() {
-
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/register' element={<Register />}></Route>
-        <Route path='/login' element={<Login />}></Route>
-        <Route path='/home' element={<Home />}></Route>
+        <Route path='/register' element={<Register />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/home' element={<Home />}/>
+        <Route path='/error' element={<Error />}/>
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
