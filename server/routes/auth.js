a7ea553cf91f3aff38b8d0408d999e634cde3f47
ChethanJS07 <jschethan75@gmail.com>
2024-07-02 19:30:28 +0530

    Modified the route of the /home directory to make your its protected from unauthorized access

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const authRouter = express.Router();
 const authController = require('../controllers/auth');
+const { authenticate } = require('../middlewares/authMiddleware');
 
 authRouter.post('/register',authController.registerUser);
 authRouter.post('/login',authController.loginUser);
+authRouter.get('/home', authenticate, (req, res) => {
+  res.send('Welcome to the Home Page');
+});
 
 module.exports = authRouter;
